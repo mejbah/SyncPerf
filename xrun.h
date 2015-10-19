d3cc6b1c43c474c4915b42b4683818e9e115d1fe
mejbah <mejbah.alam@gmail.com>
2015-10-19 15:53:17 -0500

    report conflict added

--- xrun.h
+++ xrun.h
@@ -82,7 +82,7 @@ public:
     // initialized: end the transaction (at the end of main()).
 //   	_memory.finalize();
 #ifdef REPORT
-		//report_mutex_conflicts();
+		report_mutex_conflicts();
 		report_call_site_conflicts();
 		report_thread_waits();
 #endif
